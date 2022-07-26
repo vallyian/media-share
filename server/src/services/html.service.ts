08a530525d4526830a1ed4e73078bae2f5ff393b
vallyian <vallyian@gmail.com>
2022-07-26 16:41:23 +0300

    fix html join char

--- server/src/services/html.service.ts
+++ server/src/services/html.service.ts
@@ -3,7 +3,7 @@
 export function html(items: Error | ItemStat[]): Error | string {
     return items instanceof Error
         ? items
-        : table(items.map(item => tr(td(a(item.name, item.name)) + td(item.size))).join());
+        : table(items.map(item => tr(`${td(a(item.name, item.name))}${td(item.size)}`)).join(""));
 }
 
 function table(value: string) { return `<table>${value}</table>`; }
