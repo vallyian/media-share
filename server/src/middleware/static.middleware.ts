4222a9568fa7585d776ff3028d09868e6e9a361b
vallyian <vallyian@gmail.com>
2022-08-11 17:51:04 +0300

    sanitize param script

--- server/src/middleware/static.middleware.ts
+++ server/src/middleware/static.middleware.ts
@@ -5,6 +5,7 @@ import { NextFunction, Request, Response } from "express";
 import { renderIcon } from "../services/render.service";
 import { fileExists } from "../services/fs.service";
 import { env } from "../env";
+import { paramScript } from "../services/sanitizer.service";
 
 export function favicon(_req: Request, res: Response, next: NextFunction) {
     return Promise.resolve()
@@ -17,10 +18,10 @@ export function favicon(_req: Request, res: Response, next: NextFunction) {
 }
 
 export function appScript(req: Request, res: Response, next: NextFunction) {
-    const scriptName = req.params["script"];
+    const scriptName = paramScript(req);
     let scriptPath: string;
     return Promise.resolve()
-        .then(() => scriptName || Promise.reject(`param script "${scriptName}" invalid or not set`))
+        .then(() => scriptName || Promise.reject(`param script "${req.params["script"]}" invalid or not set`))
         .then(() => scriptPath = path.join(env.VIEWS_DIR, "scripts", <string>scriptName))
         .then(() => fileExists(scriptPath))
         .then(exists => exists || Promise.reject(`script file "${scriptPath}" not found`))
