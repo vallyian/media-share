47b1ef7defe48f7d7682f4c2014b988768c3b25e
vallyian <vallyian@gmail.com>
2022-08-13 10:22:38 +0300

    finish auth middleware

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -1,27 +1,22 @@
 import { NextFunction, Request, Response } from "express";
-import { OAuth2Client, TokenPayload } from "google-auth-library";
 
 import { env } from "../env";
 import * as renderService from "../services/render.service";
-import * as cryptoService from "../services/crypto.service";
 import * as sanitizeService from "../services/sanitize.service";
+import * as authService from "../services/auth.service";
 
-type AccessToken = Pick<TokenPayload, "email">;
 
 const accessTokenCookieName = "access_token";
 
-export const authMiddleware = [verifyAccessToken, verifyIdToken, notAuthorized];
+export const authMiddleware = [verifyAccessToken, getAccessToken, getIdToken];
 
 function verifyAccessToken(req: Request, res: Response, next: NextFunction) {
     const accessToken = req.signedCookies[accessTokenCookieName];
     if (!accessToken) return next();
 
     return Promise.resolve()
-        .then(() => cryptoService.decrypt(accessToken))
-        .then(decrypted => {
-            const token: AccessToken = JSON.parse(decrypted);
-            if (!token.email) return Promise.reject("invalid access token email");
-            if (!env.G_EMAILS.includes(token.email)) return Promise.reject("email not authorized");
+        .then(() => authService.getAccessTokenPayload(accessToken))
+        .then((/* accessTokenPayload */) => {
             // populate req.user if required
             return next();
         })
@@ -31,22 +26,15 @@ function verifyAccessToken(req: Request, res: Response, next: NextFunction) {
         });
 }
 
-function verifyIdToken(req: Request, res: Response, next: NextFunction) {
+function getAccessToken(req: Request, res: Response, next: NextFunction) {
     const idToken = sanitizeService.queryParams(req).credential;
     if (!idToken) return next();
 
     const redirect = sanitizeService.queryParams(req).redirect;
 
     return Promise.resolve()
-        .then(() => new OAuth2Client(env.G_CLIENT_ID).verifyIdToken({
-            idToken,
-            audience: env.G_CLIENT_ID,
-        }))
-        .then(ticket => ticket.getPayload()
-            || Promise.reject("id token payload is undefined"))
-        .then(idToken => idToken
-            ? getAccessToken(idToken)
-            : Promise.reject("invalid id token"))
+        .then(() => authService.getIdTokenPayload(idToken))
+        .then(idToken => authService.getAccessToken(idToken))
         .then(accessToken => res
             .cookie(accessTokenCookieName, accessToken, { secure: true, signed: true, httpOnly: true, sameSite: true })
             .redirect(redirect))
@@ -56,22 +44,9 @@ function verifyIdToken(req: Request, res: Response, next: NextFunction) {
         });
 }
 
-export function notAuthorized(_req: Request, res: Response, next: NextFunction) {
+export function getIdToken(_req: Request, res: Response, next: NextFunction) {
     return Promise.resolve()
         .then(() => renderService.renderPage("auth", { gClientId: env.G_CLIENT_ID }))
         .then(({ mime, data }) => res.setHeader("Content-type", mime).status(401).end(data))
         .catch(err => next(err));
-}
-
-function getAccessToken(idToken: TokenPayload): Promise<string> {
-    return Promise.resolve()
-        .then(() => {
-            if (!idToken.email_verified) return Promise.reject("id token email not verified");
-            if (!idToken.email) return Promise.reject("invalid id token email");
-            if (!env.G_EMAILS.includes(idToken.email)) return Promise.reject("email not authorized");
-            return {
-                email: idToken.email
-            };
-        })
-        .then((at: AccessToken) => cryptoService.encrypt(JSON.stringify(at)));
 }
---
+++ server/src/services/auth.service.ts
@@ -0,0 +1,38 @@
+import gAuthLib from "google-auth-library";
+
+import { env } from "../env";
+import * as cryptoService from "../services/crypto.service";
+
+type IdTokenPayload = gAuthLib.TokenPayload;
+type AccessTokenPayload = Pick<IdTokenPayload, "email">;
+
+export function getIdTokenPayload(idToken: string): Promise<IdTokenPayload> {
+    return Promise.resolve()
+        .then(() => new gAuthLib.OAuth2Client(env.G_CLIENT_ID))
+        .then(gClient => gClient.verifyIdToken({
+            idToken,
+            audience: env.G_CLIENT_ID,
+        }))
+        .then(ticket => ticket.getPayload() || Promise.reject("id token payload is undefined"));
+}
+
+export function getAccessToken(idToken: IdTokenPayload): Promise<string> {
+    return Promise.resolve()
+        .then(() => idToken.email_verified || Promise.reject("id token email not verified"))
+        .then(() => idToken.email || Promise.reject("invalid id token email"))
+        .then(() => env.G_EMAILS.includes(<string>idToken.email) || Promise.reject("email not authorized"))
+        .then(() => (<AccessTokenPayload>{ email: idToken.email }))
+        .then(payload => JSON.stringify(payload))
+        .then(payload => cryptoService.encrypt(payload));
+}
+
+export function getAccessTokenPayload(accessToken: string): Promise<AccessTokenPayload> {
+    return Promise.resolve()
+        .then(() => cryptoService.decrypt(accessToken))
+        .then(decrypted => <Partial<AccessTokenPayload>>JSON.parse(decrypted))
+        .then(token => {
+            if (!token.email) return Promise.reject("invalid access token email");
+            if (!env.G_EMAILS.includes(token.email)) return Promise.reject("email not authorized");
+            return token;
+        });
+}
