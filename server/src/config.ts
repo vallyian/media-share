6f793f9aeae34e1938baa6c6ecc529990cd90f15
vallyian <vallyian@gmail.com>
2022-09-28 17:49:16 +0300

    fix config env validators and redo unit test

--- server/src/config.ts
+++ server/src/config.ts
@@ -9,8 +9,7 @@ export class Config {
     readonly proxyLocation: string;
     readonly rateLimitMinutes: number;
     readonly rateLimitCounter: number;
-
-    readonly mediaDir = "media";
+    readonly mediaDir: string;
 
     constructor(
         environment: Record<string, string | undefined>,
@@ -23,28 +22,29 @@ export class Config {
         this.authEmails = env("MEDIA_SHARE__AuthEmails").stringArray(v => v.split(",").map(s => s.trim()).filter(s => s.includes("@")));
         this.tokenKey = env("MEDIA_SHARE__TokenKey").string(() => randomStringFactory(32));
         this.cookieSecret = env("MEDIA_SHARE__CookieSecret").string(() => randomStringFactory(32));
-        this.port = env("MEDIA_SHARE__Port").number(() => 58082, n => n > 0 && n < 65536);
-        this.rateLimitMinutes = env("MEDIA_SHARE__RateLimitMinutes").number(() => 5, n => n > 0 && n < 24 * 60);
+        this.port = env("MEDIA_SHARE__Port").number(() => 58082, n => n > 0 && n <= 65535);
+        this.rateLimitMinutes = env("MEDIA_SHARE__RateLimitMinutes").number(() => 5, n => n > 0 && n <= 24 * 60);
         this.proxyLocation = env("MEDIA_SHARE__ProxyLocation").string(() => "/");
 
         this.clusters = this.NODE_ENV === "development" ? 1 : clustersFactory();
         this.rateLimitCounter = Math.ceil(this.rateLimitMinutes * 60 * 5 / this.clusters);
+        this.mediaDir = "media";
 
         function env(key: string) {
             return {
                 string(def?: () => string, fun?: (val: string) => boolean): string {
-                    const val = environment[key] !== undefined ? environment[key] : def ? def() : undefined;
-                    return (typeof val === "string" && (!fun || fun(val))) ? val : exit(key);
+                    const val = environment[key] ? environment[key] : def ? def() : undefined;
+                    return (typeof val === "string" && val && (!fun || fun(val))) ? val : exit(key);
                 },
 
                 number(def?: () => number, fun?: (val: number) => boolean): number {
-                    const val = environment[key] !== undefined ? environment[key] : def ? def() : undefined;
-                    return (typeof val === "number" && (!fun || fun(val))) ? val : exit(key);
+                    const val = environment[key] ? +<string>environment[key] : def ? def() : undefined;
+                    return (typeof val === "number" && isFinite(val) && (!fun || fun(val))) ? val : exit(key);
                 },
 
                 stringArray(tf: (val: string) => string[], fun?: (val: string[]) => boolean): string[] {
-                    const val = environment[key] !== undefined ? tf(<string>environment[key]) : undefined;
-                    return (val instanceof Array && (!fun || fun(val))) ? val : exit("");
+                    const val = environment[key] ? tf(<string>environment[key]) : undefined;
+                    return (val instanceof Array && val.length && (!fun || fun(val))) ? val : exit(key);
                 }
             };
         }
